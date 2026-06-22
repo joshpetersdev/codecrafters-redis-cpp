bf1541967d77647a63d1079dafa8fdef9c747498
joshpetersdev <joshuagpeters99@gmail.com>
2026-06-22 16:18:17 -0600

    codecrafters submit [skip ci]

--- src/main.cpp
+++ src/main.cpp
@@ -59,6 +59,7 @@ int main(int argc, char **argv) {
   const char *response = "+PONG\r\n";
   send(client_fd, response, strlen(response), 0);
   
+  close(client_fd);
   close(server_fd);
 
   return 0;
