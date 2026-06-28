c8fb3163993ce34acaaf47ed3f62968b2291707e
Josh Peters <joshuagpeters99@gmail.com>
2026-06-28 16:30:21 -0600

    codecrafters submit [skip ci]

--- src/main.cpp
+++ src/main.cpp
@@ -12,6 +12,9 @@
 void send_response(int client_fd) {
   char buffer[1024];
   int bytes_receveived = recv(client_fd, buffer, sizeof(buffer), 0);
+  if (bytes_receveived <= 0) {
+    close(client_fd);
+  }
   const char *response = "+PONG\r\n";
   send(client_fd, response, strlen(response), 0);
 
@@ -70,8 +73,6 @@ int main(int argc, char **argv) {
     
     if (worker.joinable())
       worker.join();
-
-    close(client_fd);
   }
 
   close(server_fd);
